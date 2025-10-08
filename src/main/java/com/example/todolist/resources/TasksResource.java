493010afcad193c96623381190fd22f7a250cd21
itoshi-m-dev <emanuelkalison@gmail.com>
2025-10-08 10:34:31 -0300

    Update Tasks, PutMapping

--- src/main/java/com/example/todolist/resources/TasksResource.java
+++ src/main/java/com/example/todolist/resources/TasksResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.todolist.entities.Tasks;
 import com.example.todolist.services.TasksService;
-
-import jakarta.servlet.Servlet;
 
 @RestController
 @RequestMapping(value = "/tasks")
@@ -47,6 +46,15 @@ public class TasksResource {
 				.buildAndExpand(obj.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(obj);
+		
+		
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Tasks> update(@PathVariable Long id, @RequestBody Tasks obj){
+		obj = service.update(id, obj);
+		return  ResponseEntity.ok().body(obj);
+	
 		
 		
 	}
