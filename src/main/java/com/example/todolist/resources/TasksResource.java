1822cd69a7221f5fe678d285feb4341f4b7f72a6
itoshi-m-dev <emanuelkalison@gmail.com>
2025-10-08 10:00:25 -0300

    Tasks FindById Resource

--- src/main/java/com/example/todolist/resources/TasksResource.java
+++ src/main/java/com/example/todolist/resources/TasksResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,14 @@ public class TasksResource {
 		List<Tasks> obj = service.findAll();
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tasks> findById(@PathVariable Long id){
+		Tasks obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	
 	}
 	
 	
